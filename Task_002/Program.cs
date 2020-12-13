932497b02df858e6370622883f64aa7ca2bad9d2
htcak <htcakuma@gmail.com>
2020-12-13 22:39:16 +0200

    Add project files.

---
+++ Task_002/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task_002
+{
+    class Program
+    {
+        static void SetPaymentValue(ref double payment) 
+        {
+            bool isCorrect = false;
+
+            while (true)
+            {
+                Console.WriteLine("Enter credit mountly payment...");
+                if (!(double.TryParse(Console.ReadLine(), out payment))) 
+                {
+                    Console.WriteLine("Wrong input please, try again later");
+                } else 
+            }
+        }
+
+        static void CalculateCreditRest(double payment) 
+        {
+            string loanIsPayed = "Loan is payed.";
+
+
+        }
+        static void Main(string[] args)
+        {
+            int creditSum = 700;
+            double payment = default;
+            SetPaymentValue(ref payment);
+            
+        }
+    }
+}
