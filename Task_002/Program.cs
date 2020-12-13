2a86a8dc22a68e51baf9304db3ec07a53ae4bb18
htcak <htcakuma@gmail.com>
2020-12-13 23:19:37 +0200

    Complete Task2

--- Task_002/Program.cs
+++ Task_002/Program.cs
@@ -6,30 +6,47 @@ namespace Task_002
     {
         static void SetPaymentValue(ref double payment) 
         {
-            bool isCorrect = false;
-
             while (true)
             {
                 Console.WriteLine("Enter credit mountly payment...");
-                if (!(double.TryParse(Console.ReadLine(), out payment))) 
+                if ((!(double.TryParse(Console.ReadLine(), out payment))) || payment < 100)
                 {
                     Console.WriteLine("Wrong input please, try again later");
-                } else 
+                }
+                else break;
             }
         }
 
-        static void CalculateCreditRest(double payment) 
+        static void CalculateCreditRest(double payment, int creditAmount = 700) 
         {
-            string loanIsPayed = "Loan is payed.";
+            string loanIsPayed = "Loan is payed.\n";
+            string loanIsUnpaid = "loan is unpaid yet\n";
+            double restPayment = creditAmount - payment;
+            
+            if (restPayment > 0) 
+            {
+                Console.WriteLine(loanIsUnpaid);
+                Console.WriteLine($"Loan rest is {restPayment} UAH");
+                Console.WriteLine($"Next mounthly payment is {restPayment / 6} UAH");
+            }
 
+            if (restPayment == 0) 
+            {
+                Console.WriteLine(loanIsPayed);
+            }
 
+            if (restPayment < 0)
+            {
+                Console.WriteLine(loanIsPayed);
+                Console.WriteLine($"Overpayment is {Math.Abs(restPayment)} UAH");
+            }
         }
         static void Main(string[] args)
         {
-            int creditSum = 700;
             double payment = default;
             SetPaymentValue(ref payment);
-            
+            CalculateCreditRest(payment);
+            Console.ReadKey();
         }
     }
 }
